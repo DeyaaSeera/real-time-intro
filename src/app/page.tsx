ba71fe1934c5846fbf3323da13b9a8ddf664c617
Reham Habbas <reham.habbas@ip-10-72-52-1.eu-west-1.compute.internal>
2024-07-18 09:35:28 +0400

    feat: polling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Header from "./components/Header/Header";
+import LongPollingComponent from "./components/LongPollingComponent/LongPollingComponent";
 import MatchList from "./components/MatchList/MatchList";
+import ShortPollingComponent from "./components/ShortPollingComponent/ShortPollingComponent";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <MatchList />
+      {/* <MatchList /> */}
+      <ShortPollingComponent />
+      {/* <LongPollingComponent /> */}
     </>
   );
 }
